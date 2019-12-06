766c3354fb26a6410c253c43eae08719171f8045
Archaeologist03 <omniiknight@hotmail.com>
2019-12-06 20:25:13 +0100

    User Registration

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from 'react';
+import axios from 'axios';
 
 import AuthContext from './authContext';
 import AuthReducer from './authReducer';
@@ -25,23 +26,50 @@ const AuthState = props => {
 	const [state, dispatch] = useReducer(AuthReducer, initalState);
 
 	// Load User
+	const loadUser = () => {};
 
 	// Register User
+	const register = async formData => {
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		};
+
+		try {
+			const res = await axios.post('/api/users', formData, config);
+
+			dispatch({
+				type: REGISTER_SUCCESS,
+				payload: res.data,
+			});
+		} catch (err) {
+			dispatch({
+				type: REGISTER_FAIL,
+				payload: err.response.data.msg,
+			});
+		}
+	};
 
 	// Login User
+	const login = () => {};
 
 	// Logout
+	const logout = () => {};
 
 	// Clear Errors
+	const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
 
 	return (
 		<AuthContext.Provider
 			value={{
 				token: state.token,
-				isAuthenticated: state.token,
+				isAuthenticated: state.isAuthenticated,
 				loading: state.loading,
 				user: state.user,
 				error: state.error,
+				register,
+				clearErrors,
 			}}>
 			{props.children}
 		</AuthContext.Provider>
